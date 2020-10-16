70a58873b4a7199cbe1f79ee3d50a40e41e21c25
nullchinchilla <nullchinchilla@pm.me>
2020-10-16 09:53:24 -0500

    fix CI

--- osx_build.sh
+++ osx_build.sh
@@ -18,7 +18,7 @@ cd osxcross
 wget -nc https://s3.dockerproject.org/darwin/v2/MacOSX10.10.sdk.tar.xz
 mv MacOSX10.10.sdk.tar.xz tarballs/
 UNATTENDED=yes OSX_VERSION_MIN=10.7 ./build.sh
-PATH="$(pwd)/osxcross/target/bin:$PATH" \
+PATH="$(pwd)/target/bin:$PATH" \
 CC=o64-clang \
 CXX=o64-clang++ \
 cargo build --release --target x86_64-apple-darwin --manifest-path=../geph4-client/Cargo.toml
