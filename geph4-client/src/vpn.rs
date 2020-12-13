8fc826db067995aea7eb49342f4f65559e4bf408
nullchinchilla <nullchinchilla@pm.me>
2020-12-12 18:22:29 -0600

    fix DNS transparent proxy. This wasn't as trivial as it seemed

--- geph4-client/src/vpn.rs
+++ geph4-client/src/vpn.rs
@@ -1,20 +1,22 @@
-use std::{io::Stdin, num::NonZeroU32, sync::Arc, time::Duration};
+use std::{collections::HashMap, io::Stdin, num::NonZeroU32, sync::Arc, time::Duration};
 
 use async_net::Ipv4Addr;
 use bytes::Bytes;
 use governor::Quota;
 use once_cell::sync::Lazy;
+use parking_lot::RwLock;
 use pnet_packet::{
     ip::IpNextHeaderProtocols,
     ipv4::{Ipv4Packet, MutableIpv4Packet},
     tcp::{TcpFlags, TcpPacket},
+    udp::MutableUdpPacket,
     udp::UdpPacket,
     Packet,
 };
 use smol_timeout::TimeoutExt;
 use vpn_structs::StdioMsg;
 
-use crate::{stats::StatCollector, GEXEC};
+use crate::stats::StatCollector;
 
 /// runs a vpn session
 pub async fn run_vpn(
@@ -56,9 +58,12 @@ pub async fn run_vpn(
         stdout.flush()?;
     }
 
+    // a mini-nat for DNS request
+    let dns_nat = RwLock::new(HashMap::new());
     let vpn_up_fut = {
         let mux = mux.clone();
         let stats = stats.clone();
+        let dns_nat = &dns_nat;
         async move {
             let ack_rate_limits: Vec<_> = (0..16)
                 .map(|_| {
@@ -77,7 +82,7 @@ pub async fn run_vpn(
                     }
                 }
                 // fix dns
-                let body = if let Some(body) = fix_dns_dest(&msg.body) {
+                let body = if let Some(body) = fix_dns_dest(&msg.body, dns_nat) {
                     body
                 } else {
                     msg.body
@@ -96,6 +101,7 @@ pub async fn run_vpn(
     };
     let vpn_down_fut = {
         let stats = stats.clone();
+        let dns_nat = &dns_nat;
         async move {
             for count in 0u64.. {
                 if count % 1000 == 0 {
@@ -116,6 +122,11 @@ pub async fn run_vpn(
                 let bts = mux.recv_urel().await?;
                 if let vpn_structs::Message::Payload(bts) = bincode::deserialize(&bts)? {
                     stats.incr_total_rx(bts.len() as u64);
+                    let bts = if let Some(bts) = fix_dns_src(&bts, dns_nat) {
+                        bts
+                    } else {
+                        bts
+                    };
                     let msg = StdioMsg { verb: 0, body: bts };
                     {
                         use std::io::Write;
@@ -128,7 +139,7 @@ pub async fn run_vpn(
             unreachable!()
         }
     };
-    smol::future::race(GEXEC.spawn(vpn_up_fut), GEXEC.spawn(vpn_down_fut)).await
+    smol::future::race(vpn_up_fut, vpn_down_fut).await
 }
 
 /// returns ok if it's an ack that needs to be decimated
@@ -145,21 +156,63 @@ fn ack_decimate(bts: &[u8]) -> Option<u16> {
 }
 
 /// fixes dns destination
-fn fix_dns_dest(bts: &[u8]) -> Option<Bytes> {
-    let is_dns = {
+fn fix_dns_dest(bts: &[u8], nat: &RwLock<HashMap<u16, Ipv4Addr>>) -> Option<Bytes> {
+    let dns_src_port = {
         let parsed = Ipv4Packet::new(bts)?;
         if parsed.get_next_level_protocol() == IpNextHeaderProtocols::Udp {
             let parsed = UdpPacket::new(parsed.payload())?;
-            parsed.get_destination() == 53
+            if parsed.get_destination() == 53 {
+                parsed.get_source()
+            } else {
+                return None;
+            }
         } else {
-            false
+            return None;
         }
     };
-    if is_dns {
-        let mut vv = bts.to_vec();
-        let mut parsed = MutableIpv4Packet::new(&mut vv)?;
-        parsed.set_destination(Ipv4Addr::new(1, 1, 1, 1));
-        return Some(vv.into());
+    let mut vv = bts.to_vec();
+    let mut parsed = MutableIpv4Packet::new(&mut vv)?;
+    nat.write().insert(dns_src_port, parsed.get_destination());
+    parsed.set_destination(Ipv4Addr::new(74, 82, 42, 42));
+    fix_all_checksums(&mut vv)?;
+    Some(vv.into())
+}
+
+fn fix_all_checksums(bts: &mut [u8]) -> Option<()> {
+    let mut ip_layer = MutableIpv4Packet::new(bts)?;
+    let mut udp_raw = ip_layer.payload().to_vec();
+    {
+        let mut udp_layer = MutableUdpPacket::new(&mut udp_raw)?;
+        let source = ip_layer.get_source();
+        let dest = ip_layer.get_destination();
+        let udp_checksum =
+            pnet_packet::udp::ipv4_checksum(&udp_layer.to_immutable(), &source, &dest);
+        udp_layer.set_checksum(udp_checksum);
     }
-    None
+    ip_layer.set_payload(&udp_raw);
+    let ip_checksum = pnet_packet::ipv4::checksum(&ip_layer.to_immutable());
+    ip_layer.set_checksum(ip_checksum);
+    Some(())
+}
+
+/// fixes dns source
+fn fix_dns_src(bts: &[u8], nat: &RwLock<HashMap<u16, Ipv4Addr>>) -> Option<Bytes> {
+    let dns_src_port = {
+        let parsed = Ipv4Packet::new(bts)?;
+        if parsed.get_next_level_protocol() == IpNextHeaderProtocols::Udp {
+            let parsed = UdpPacket::new(parsed.payload())?;
+            if parsed.get_source() == 53 {
+                parsed.get_destination()
+            } else {
+                return None;
+            }
+        } else {
+            return None;
+        }
+    };
+    let mut vv = bts.to_vec();
+    let mut parsed = MutableIpv4Packet::new(&mut vv)?;
+    parsed.set_source(*nat.read().get(&dns_src_port)?);
+    fix_all_checksums(&mut vv)?;
+    Some(vv.into())
 }
