d1b5670e5b63d1e6199245214d188c41ba76cc80
nullchinchilla <nullchinchilla@pm.me>
2021-09-03 18:17:20 -0500

    fix

--- src/fd_semaphore.rs
+++ src/fd_semaphore.rs
@@ -5,7 +5,11 @@ use smol_timeout::TimeoutExt;
 use std::{sync::Arc, time::Duration};
 
 // limit the number of fds to a very low number to avoid running out
+#[cfg(target_os = "macos")]
 static FD_SEMAPHORE: Lazy<Arc<Semaphore>> = Lazy::new(|| Arc::new(Semaphore::new(64)));
+
+#[cfg(not(target_os = "macos"))]
+static FD_SEMAPHORE: Lazy<Arc<Semaphore>> = Lazy::new(|| Arc::new(Semaphore::new(512)));
 
 /// Blocks until a file descriptor is free, then acquires a guard.
 pub(crate) async fn acquire_fd() -> anyhow::Result<SemaphoreGuardArc> {
