6dc06cd494a442cff89fc936263198e734c1572f
nullchinchilla <nullchinchilla@pm.me>
2021-09-09 14:22:56 -0500

    stupid typo

--- move_built.sh
+++ move_built.sh
@@ -7,6 +7,6 @@ mv ./target/armv7-unknown-linux-musleabihf/release/geph4-client ./OUTPUT/$VERSIO
 mv ./target/armv7-linux-androideabi/release/geph4-client ./OUTPUT/$VERSION/geph4-client-android-armv7
 mv ./target/aarch64-linux-android/release/geph4-client ./OUTPUT/$VERSION/geph4-client-android-aarch64
 mv ./target/x86_64-pc-windows-gnu/release/geph4-client.exe ./OUTPUT/$VERSION/geph4-client-windows-amd64.exe
-mv ./target/i686-pc-windows-gnu/release/geph4-client.exe ./OUTPUT/geph4-client-windows-i386.exe
-mv ./target/x86_64-apple-darwin/release/geph4-client ./OUTPUT/geph4-client-macos-amd64
+mv ./target/i686-pc-windows-gnu/release/geph4-client.exe ./OUTPUT/$VERSION/geph4-client-windows-i386.exe
+mv ./target/x86_64-apple-darwin/release/geph4-client ./OUTPUT/$VERSION/geph4-client-macos-amd64
 exit 0
