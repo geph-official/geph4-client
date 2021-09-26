0720de638bc24346ff80aec4d3e7f578e9943efe
nullchinchilla <nullchinchilla@pm.me>
2021-09-26 18:51:47 -0500

    fix

--- src/fd_semaphore.rs
+++ src/fd_semaphore.rs
@@ -8,7 +8,7 @@ use std::sync::Arc;
 #[cfg(unix)]
 static FD_LIMIT: Lazy<usize> = Lazy::new(|| {
     let ((largest_low, _), _) = binary_search::binary_search((1, ()), (65536, ()), |lim| {
-        if rlimit::utils::increase_nofile_limit(lim).unwrap_or_default() == lim {
+        if rlimit::utils::increase_nofile_limit(lim).unwrap_or_default() >= lim {
             Direction::Low(())
         } else {
             Direction::High(())
