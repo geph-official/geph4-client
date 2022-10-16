0e3bb32be70212e305b0ce283705ebd06d79c834
nullchinchilla <nullchinchilla@pm.me>
2022-10-15 21:54:49 -0500

    fix more

--- move_built.sh
+++ move_built.sh
@@ -7,5 +7,6 @@ mv ./target/armv7-unknown-linux-musleabihf/release/geph4-client ./OUTPUT/$VERSIO
 mv ./target/armv7-linux-androideabi/release/geph4-client ./OUTPUT/$VERSION/geph4-client-android-armv7
 mv ./target/aarch64-linux-android/release/geph4-client ./OUTPUT/$VERSION/geph4-client-android-aarch64
 mv ./target/i686-pc-windows-msvc/release/geph4-client.exe ./OUTPUT/$VERSION/geph4-client-windows-i386.exe
+mv ./target/release/geph4-client.exe ./OUTPUT/$VERSION/geph4-client-windows-i386.exe
 mv ./target/x86_64-apple-darwin/release/geph4-client ./OUTPUT/$VERSION/geph4-client-macos-amd64
 exit 0
