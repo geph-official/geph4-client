de6ec614df1becfb9ff55f84f4e70c7e049d5f7e
nullchinchilla <nullchinchilla@pm.me>
2023-10-14 11:22:43 -0400

    Update target architecture from i386 to i686 for Android build in CircleCI configuration and related scripts

--- cross-setup.sh
+++ cross-setup.sh
@@ -14,7 +14,7 @@ cargo build-docker-image x86_64-linux-android \
   --build-arg ANDROID_NDK=r25b \
   --build-arg ANDROID_SDK=21 \
   --build-arg ANDROID_VERSION=5.0.0_r1
-cargo build-docker-image i386-linux-android \
+cargo build-docker-image i686-linux-android \
   --build-arg ANDROID_NDK=r25b \
   --build-arg ANDROID_SDK=21 \
   --build-arg ANDROID_VERSION=5.0.0_r1
