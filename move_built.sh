8eba3a5caf5ab8be5e5efb1b58d0eb7d2af9a28e
nullchinchilla <nullchinchilla@pm.me>
2023-10-14 09:45:52 -0400

    Add support for building x86_64 and i386 Android versions and fix versioning to use latest tag

--- move_built.sh
+++ move_built.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
-VERSION=$(git describe)
+VERSION=$(git describe --tags `git rev-list --tags --max-count=1`)
 mkdir -p ./OUTPUT/$VERSION/
 mv ./target/x86_64-unknown-linux-musl/release/geph4-client ./OUTPUT/$VERSION/geph4-client-linux-amd64
 mv ./target/x86_64-unknown-linux-gnu/release/geph4-vpn-helper ./OUTPUT/$VERSION/geph4-vpn-helper-linux-amd64
 mv ./target/armv7-unknown-linux-musleabihf/release/geph4-client ./OUTPUT/$VERSION/geph4-client-linux-armv7
 mv ./target/armv7-linux-androideabi/release/geph4-client ./OUTPUT/$VERSION/geph4-client-android-armv7
 mv ./target/aarch64-linux-android/release/geph4-client ./OUTPUT/$VERSION/geph4-client-android-aarch64
+mv ./target/x86_64-linux-android/release/geph4-client ./OUTPUT/$VERSION/geph4-client-android-amd64
+mv ./target/i386-linux-android/release/geph4-client ./OUTPUT/$VERSION/geph4-client-android-i386
 mv ./target/i686-pc-windows-msvc/release/geph4-client.exe ./OUTPUT/$VERSION/geph4-client-windows-i386.exe
 mv ./target/release/geph4-client.exe ./OUTPUT/$VERSION/geph4-client-windows-i386.exe
 mv ./target/x86_64-apple-darwin/release/geph4-client ./OUTPUT/$VERSION/geph4-client-macos-amd64
