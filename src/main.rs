8ffa633a16d0a7d41373e9c53608b9eef27de064
nullchinchilla <nullchinchilla@pm.me>
2023-10-29 09:25:42 -0400

    Enable single-threaded mode in main function using smolscale

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use binary_search::Direction;
 use geph4client::dispatch;
 
 fn main() -> anyhow::Result<()> {
+    smolscale::permanently_single_threaded();
     let ((largest_low, _), _) = binary_search::binary_search((1, ()), (65536, ()), |lim| {
         if rlimit::utils::increase_nofile_limit(lim).unwrap_or_default() >= lim {
             Direction::Low(())
