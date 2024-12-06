ef9aa550b682d0525d29d30f6905907cc85a58db
nullchinchilla <nullchinchilla@pm.me>
2024-12-06 11:45:03 -0500

    Update geph5-client to version 0.2.23 and remove unused cfg directive

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use binary_search::Direction;
 use geph4client::dispatch;
 
 fn main() -> anyhow::Result<()> {
-    #[cfg(target_os = "android")]
     smolscale::permanently_single_threaded();
     let ((largest_low, _), _) = binary_search::binary_search((1, ()), (65536, ()), |lim| {
         if rlimit::utils::increase_nofile_limit(lim).unwrap_or_default() >= lim {
